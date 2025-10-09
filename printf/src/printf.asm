373a74acd59cf0c97a91715f5e35bd3d7f9cd551
Rustatano <rustatano@gmail.com>
2025-10-09 11:42:55 +0200

    print_char, progress in  print_decimal

--- printf/src/printf.asm
+++ printf/src/printf.asm
@@ -12,10 +12,20 @@ section .text
     global printf
 
 printf:
-    mov     ecx, [esp + 8]              ; load format_string to ecx
+    ;mov     ecx, [esp + 8]              ; load format_string to ecx
+
+    pop     ebx                         ; function address
+    pop     eax                         ; load value to print to EAX
+    pop     ecx                         ; load formattingn string to ECX
+    push    ebx                         ; push afunction address back
 
     call    get_print_type
-    
+
+    call    print_irq
+
+    ret
+
+print_irq:
     mov     eax, 4                      ; sys_write
     mov     ebx, 1                      ; stdout
     int     80h                         ; print
@@ -32,11 +42,14 @@ get_print_type:
 
     cmp     byte [ecx], 64h             ; compare to 'd' char, decimal
     je      print_decimal               ; jump to print_decimal if fmt_str = "%d"
+
+    cmp     byte [ecx], 63h             ; compare to 'c' char, decimal
+    je      print_char                  ; jump to print_char if fmt_str = "%c"
     
     ret
 
 print_string:
-    mov     ecx, [esp + 8]              ; the arg is on esp + 4, but calling function pushes 
+    mov     ecx, eax                    ; move value to print from EAX, to ecx 
                                         ;   it's address to stack and ret pops it back, so it 
                                         ;   needs to be 4 bytes more,
                                         ;   jump instruction doesn't push anything to stack
@@ -50,8 +63,50 @@ print_string:
     
     ret
 
+print_char:
+    mov     ecx, eax                    ; move char to ECX
+    mov     edx, 1                      ; set output length to one
+
+    ret
+
 print_decimal:
+    ; 10 ** 1       |   10 ** 2             |   10 ** 3                 | 10 ** 4               | 
+    ; 1530 z 8      |   1500 z 30 / 10 ** 1 |   1000 z 500 / 10 ** 2    | 0 z 1000 / 10 ** 3    | end of loop if quotient == 0
     
+    ; loop
+    ; ecx = 10
+    ; 
+    ; eax / ecx ** esi = eax, z edx
+    ; 
+    ; eax = char edx
+    ; edx = 1
+    ; push ebx
+    ; print
+    ; jmp loop
+
+    ; edx - remainder
+    ; mov     eax, 102
+    ; mov     edx, 0
+    ; mov     ecx, 10
+    ; div     ecx
+    ; eax -> 10
+    ; edx -> 2
+        
+    mov     ecx, 10 ; divisor
+    mov     esi, 10 ; exponent
+    
+    div     esi
+
+    add     edx, 48
+    push    edx
+    pop     ecx
+    mov     edx, 1
+
+    call    print_irq
+
+    mul     esi     ; power +1
+    
+
 
     ; 1538 -> 1000, 500, 30, 8
     ; get num size
