ee28329df5e3c234db6c137db2bbc16f57247f24
Rustatano <rustatano@gmail.com>
2025-10-10 22:33:52 +0200

    remove comment

--- _test/src/main.asm
+++ _test/src/main.asm
@@ -37,17 +37,3 @@ _start:
     mov     eax, 1                      ; exit
     mov     ebx, 0
     int     80h
-
-; -----int printing principle ---------
-;mov     ecx, [num]      ; load from variable
-;add     ecx, 4          ; here do something with the number
-;add     ecx, 48         ; make it char, from ascii table
-;mov     [num], ecx      ; save to variable
-;push    num             
-
-;mov     eax, 4
-;mov     ebx, 1
-;pop     ecx
-;mov     edx, 1
-;int     80h
-; -------------------------------------
