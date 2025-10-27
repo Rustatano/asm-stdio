e9569f1b74c9c73aa8f29c200c9788a6a810328f
Rustatano <rustatano@gmail.com>
2025-10-27 19:17:24 +0100

    trying to remove newline char

--- scanf_s/src/scanf_s.asm
+++ scanf_s/src/scanf_s.asm
@@ -2,57 +2,71 @@
 ; Can be variable length
 
 section .bss
-    input resb 8
+input resb 8
 
 section .text
-    global scanf_s
+global scanf_s
 
 scanf_s:
-    push    ebp                         ; save registers
-    mov     ebp, esp
-    push    eax
-    push    ebx
-    push    ecx
-    push    edx
+push    ebp                             ; save registers
+mov     ebp, esp
+push    eax
+push    ebx
+push    ecx
+push    edx
 
-    mov     eax, [ebp + 8]              ; move pointer to fmt string argument
+mov     eax, [ebp + 8]                  ; move pointer to fmt string argument
 
-    cmp     byte [eax], 25h             ; compare first byte to '%' char
-    jne     exit
+cmp     byte [eax], 25h                 ; compare first byte to '%' char
+jne     exit
 
-    inc     eax                         ; move pointer to next char in fmt string
+inc     eax                             ; move pointer to next char in fmt string
 
-    cmp     byte [eax], 64h             ; compare to 'd' char, decimal
-    je      scan_decimal                ; TODO: scan decimal, parsing strin to number
-    cmp     byte [eax], 63h             ; compare to 'c' char, char
-    je      scan_string_char
-    cmp     byte [eax], 73h             ; compare to 's' char, string
-    je      scan_string_char
+cmp     byte [eax], 64h                 ; compare to 'd' char, decimal
+je      scan_decimal                    ; TODO: scan decimal, parsing strin to number
+cmp     byte [eax], 63h                 ; compare to 'c' char, char
+je      scan_string_char
+cmp     byte [eax], 73h                 ; compare to 's' char, string
+je      scan_string_char
 
-    scan_decimal:
-    jmp     exit
-    scan_string_char:
-    mov     eax, 3                      ; sys_read
-    mov     ebx, 0                      ; stdin
-    ;mov     ecx, [ebp + 12]             ; address to save the input to
-    mov     ecx, input                  ; load pointer to 'input' variable
-    int     80h
+scan_decimal:
+jmp     exit
+scan_string_char:
+mov     eax, 3                          ; sys_read
+mov     ebx, 0                          ; stdin
+mov     ecx, input                      ; load pointer to 'input' variable
+int     80h
 
-    mov     ecx, [ecx]                  ; get value on 'input' address saved in ECX
-    mov     eax, [ebp + 12]             ; load pointer to strin variable on stack
-    mov     [eax], ecx                  ; set value on address in EAX to value in ECX
+mov     ecx, [ecx]                      ; get value on 'input' address saved in ECX
+mov     eax, [ebp + 12]                 ; load pointer to string variable on stack
+mov     [eax], ecx                      ; set value on address in EAX to value in ECX
 
+je      remove_newline_char
 
-    ; TODO: remove newline char in scanned string
+; TODO: remove newline char in scanned string
 
-    exit:
-    pop     edx                         ; retrive registers
-    pop     ecx
-    pop     ebx
-    pop     eax
-    pop     ebp
+exit:
+pop     edx                             ; retrive registers
+pop     ecx
+pop     ebx
+pop     eax
+pop     ebp
 
-    ret                                 ; return value is in EAX
+ret                                     ; return value is in EAX
+
+remove_newline_char:
+;mov     eax, [eax]
+
+l1:
+cmp     byte [eax], 0ah                  ; compare string char to newline char
+inc     eax                             ; move to next char in string
+jne     l1             ; jump to next loop iteration
+
+dec     eax                             ; move back to newline char
+mov     ebx, 0x0
+mov     [eax], ebx                      ; move nul char to newline char position
+
+jmp     exit
 
 section .note.GNU-stack \
     noalloc noexec nowrite progbits
